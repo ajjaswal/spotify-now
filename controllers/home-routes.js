61d911905a3f48bfee24dbf97250973acfd03dce
Chris <chrismanfredijr@gmail.com>
2023-01-11 18:10:44 -0500

    new callback url

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,7 +10,7 @@ const SpotifyWebApi = require("spotify-web-api-node");
 const spotifyApi = new SpotifyWebApi({
    clientId: "be6d6cea500242db91d8960be9638a5d",
    clientSecret: "2c1169e6b3cb44359989797fddc1954f",
-   redirectUri: "https://spotify-wrapped-now.herokuapp.com/callback",
+   redirectUri: "https://spotify-now-app.herokuapp.com/callback",
 });
 
 // what data we want to access
